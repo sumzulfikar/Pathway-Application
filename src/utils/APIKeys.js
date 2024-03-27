bcfa3e34fb58c7df1414a6110ce027d634c06c75
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-03-27 11:15:33 +0000

    test

---
+++ src/utils/APIKeys.js
@@ -0,0 +1,8 @@
+const appID = "07eb7ed2";
+const api_Key = "8b62f36700cfa5fc340b8fce79dbc165";
+const apiAutoCompleteKey = "UPtaZvBhLYhragYNSYaucjj1vytI_j9X3vFoyU1HGJk";
+const googleMapsApiKey = "AIzaSyAsMvdELIPtsdYB3qYA_s8vUTAzOKP5ALU";
+
+
+export { appID, api_Key, apiAutoCompleteKey, googleMapsApiKey };
+
